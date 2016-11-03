2fc4fdb37b5d72100dc137d063b4428dba9d7a43
dcodeIO <dcode@dcode.io>
2016-11-03 03:15:03 +0100

    Reposition the important parts in index.js

--- src/index.js
+++ src/index.js
@@ -2,32 +2,6 @@ var util = require("./util");
 
 var protobuf = module.exports = {};
 
-// parser
-protobuf.tokenize = require("./tokenize");
-protobuf.parse = require("./parse");
-
-// serialization
-protobuf.Writer = require("./writer");
-protobuf.BufferWriter = protobuf.Writer.BufferWriter;
-protobuf.Reader = require("./reader");
-protobuf.BufferReader = protobuf.Reader.BufferReader;
-
-// reflection
-protobuf.ReflectionObject = require("./object");
-protobuf.Namespace = require("./namespace");
-protobuf.Root = require("./root");
-protobuf.Type = require("./type");
-protobuf.Field = require("./field");
-protobuf.Enum = require("./enum");
-protobuf.Service = require("./service");
-protobuf.Method = require("./method");
-
-// runtime
-protobuf.Prototype = require("./prototype");
-
-// utility
-protobuf.util = util;
-
 /**
  * Loads one or multiple .proto files into a common root namespace.
  * @param {string|!Array.<string>} filename One or multiple files to load
@@ -47,3 +21,29 @@ protobuf.load = function load(filename, root, callback, ctx) {
         root = new protobuf.Root();
     return root.load(filename, callback, ctx);
 };
+
+// Parser
+protobuf.tokenize = require("./tokenize");
+protobuf.parse = require("./parse");
+
+// Serialization
+protobuf.Writer = require("./writer");
+protobuf.BufferWriter = protobuf.Writer.BufferWriter;
+protobuf.Reader = require("./reader");
+protobuf.BufferReader = protobuf.Reader.BufferReader;
+
+// Reflection
+protobuf.ReflectionObject = require("./object");
+protobuf.Namespace = require("./namespace");
+protobuf.Root = require("./root");
+protobuf.Type = require("./type");
+protobuf.Field = require("./field");
+protobuf.Enum = require("./enum");
+protobuf.Service = require("./service");
+protobuf.Method = require("./method");
+
+// Runtime
+protobuf.Prototype = require("./prototype");
+
+// Utility
+protobuf.util = util;
