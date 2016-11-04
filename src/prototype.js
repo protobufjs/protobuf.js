f83fadf60c5f86077b4478da7456cf13408adb52
dcodeIO <dcode@dcode.io>
2016-11-04 05:00:57 +0100

    Additional options when extending

--- src/prototype.js
+++ src/prototype.js
@@ -25,37 +25,43 @@ function Prototype(properties) {
  * Makes the specified constructor extend this prototype.
  * @param {function(new:Message)} constructor Constructor to extend
  * @param {!type} type Message type
+ * @param {!Object.<string,*>} [options] Additional options
  * @returns {!Object} Prototype
  */
-Prototype.extend = function extend(constructor, type) {
+Prototype.extend = function extend(constructor, type, options) {
+    options = options || {};
 
-    // Underlying reflected type for reference
-    constructor.$type = type;
+    if (!options.noStatics) {
+            
+        // Underlying reflected type for reference
+        constructor.$type = type;
 
-    // Create a new message
-    constructor.create = function(properties) {
-        return this.$type.create(properties, constructor);
-    };
+        // Create a new message
+        constructor.create = function(properties) {
+            return this.$type.create(properties, constructor);
+        };
 
-    // Encode to a buffer directly
-    constructor.encode = function encode(message) {
-        return this.$type.encode(message).finish();
-    };
+        // Encode to a buffer directly
+        constructor.encode = function encode(message) {
+            return this.$type.encode(message).finish();
+        };
 
-    // Encode to a buffer directly, length delimited
-    constructor.encodeDelimited = function encodeDelimited(message) {
-        return this.$type.encodeDelimited(message).finish();
-    };
+        // Encode to a buffer directly, length delimited
+        constructor.encodeDelimited = function encodeDelimited(message) {
+            return this.$type.encodeDelimited(message).finish();
+        };
 
-    // Decode from a buffer
-    constructor.decode = function decode(buffer) {
-        return this.$type.decode(buffer, constructor);
-    };
+        // Decode from a buffer
+        constructor.decode = function decode(buffer) {
+            return this.$type.decode(buffer, constructor);
+        };
 
-    // Decode from a buffer, length delimited
-    constructor.decodeDelimited = function decodeDelimited(buffer) {
-        return this.$type.decodeDelimited(buffer, constructor);
-    };
+        // Decode from a buffer, length delimited
+        constructor.decodeDelimited = function decodeDelimited(buffer) {
+            return this.$type.decodeDelimited(buffer, constructor);
+        };
+
+    }
 
     // Properly extend Message
     var prototype = constructor.prototype = new Prototype();
@@ -66,8 +72,8 @@ Prototype.extend = function extend(constructor, type) {
         type.each(function(field, name) {
             if (field.repeated)
                 return;
-            this[name] = field.defaultValue; // eslint-disable-line no-invalid-this
-        }, prototype, type.fields);
+            prototype[name] = field.defaultValue; // eslint-disable-line no-invalid-this
+        }, type, type.fields);
 
     return prototype;
 };
