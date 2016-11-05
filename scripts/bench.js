714b6fd03660807c8dd5b879ff66cf3ddbf759b1
dcodeIO <dcode@dcode.io>
2016-11-05 06:34:18 +0100

    Bench again non-native JSON, too

--- scripts/bench.js
+++ scripts/bench.js
@@ -1,4 +1,10 @@
-var protobuf = require("..");
+var protobuf = require(".."),
+    nativeStringify = JSON.stringify;
+var JSON3 = require("./lib/json3");
+
+if (JSON3.stringify === nativeStringify)
+    throw Error("something went wrong");
+
 var pkg = require(__dirname + "/../package.json");
 
 protobuf.load(__dirname + "/../tests/data/package.proto", function(err, root) {
@@ -9,7 +15,7 @@ protobuf.load(__dirname + "/../tests/data/package.proto", function(err, root) {
 
         // NOTE: This is currently testing string-heavy data, exactly where JSON excels.
         // On my machine, protobuf is 6 times slower, which isn't bad considering that JSON
-        // is all native and heavily optimized.
+        // is all native and heavily optimized. Compared with JSON3, protobuf is slightly faster.
 
         // Our goal should be to reduce function calls and reflection iterations to a minimum.
         // This is where code generation comes into play.
@@ -18,6 +24,8 @@ protobuf.load(__dirname + "/../tests/data/package.proto", function(err, root) {
         var myPackage = Package.decode(Package.encode(Package.create(pkg)).finish());
         
         var times = 50000;
+
+        console.log("running " + times + " iterations ...");
 
         function bench_protobuf() {
             var start = Date.now(),
@@ -30,7 +38,7 @@ protobuf.load(__dirname + "/../tests/data/package.proto", function(err, root) {
             console.log("protobuf encode/decode: " + (Date.now() - start) + "ms (" + len + " bytes)");
         }
 
-        function bench_json() {
+        function bench_json_native() {
             var start = Date.now(),
                 len = 0;
             for (var i = 0; i < times; ++i) {
@@ -38,11 +46,23 @@ protobuf.load(__dirname + "/../tests/data/package.proto", function(err, root) {
                 JSON.parse(buf.toString("utf8"));
                 len += buf.length;
             }
-            console.log("JSON stringify/parse: " + (Date.now() - start) + "ms (" + len + " bytes)");
+            console.log("JSON [native] stringify/parse: " + (Date.now() - start) + "ms (" + len + " bytes)");
         }
 
-        bench_json();
+        function bench_json3() {
+            var start = Date.now(),
+                len = 0;
+            for (var i = 0; i < times; ++i) {
+                var buf = Buffer.from(JSON3.stringify(pkg), "utf8");
+                JSON3.parse(buf.toString("utf8"));
+                len += buf.length;
+            }
+            console.log("JSON3 stringify/parse: " + (Date.now() - start) + "ms (" + len + " bytes)");
+        }
+
         bench_protobuf();
+        bench_json_native();
+        bench_json3();
 
     } catch (e) {
         console.error(e);
