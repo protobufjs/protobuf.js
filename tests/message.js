5f9bede280aa998afb7898e8d2718b4a229e8e6f
Conor Branagan <conor.branagan@gmail.com>
2016-12-14 17:07:17 -0500

    Fix asJSON defaults option, make it work for repeated fields.
    Fixing two issues in here:
    
    1. When the `defaults` option was enabled, we were using `Object.keys(this)`
       which ends up returning an array that includes things like `$root` and
       asJSON. Instead we can use the `fields` we already have available.
    
    2. For repeated values we were not setting the empty list default because the
       length check would break earlier. We're now passing through when the
       defaults option is set as well.
    
    Added a test on this stuff as well.

---
+++ tests/message.js
@@ -0,0 +1,25 @@
+var tape = require("tape");
+var protobuf  = require("..");
+
+tape.test("test asJSON repeated defaults", function(test) {
+
+    protobuf.load("tests/data/message.proto", function(err, root) {
+        if (err)
+            return test.fail(err.message);
+        test.ok(true, "should parse without errors");
+
+        var TestMessage = root.lookup("test.TestRepeatedDefaults");
+        var msg1 = TestMessage.create({ aString: 'foo' });
+
+        var defaultsOn = msg1.asJSON({ defaults: true });
+        test.equal(defaultsOn.aString, 'foo', "should set aString value");
+        test.same(defaultsOn.repeatString, [], "should set repeated default");
+
+        var defaultsOff = msg1.asJSON({ defaults: false });
+        test.equal(defaultsOff.aString, 'foo', "should set aString value");
+        test.same(defaultsOff.repeatString, undefined, "should not set repeated default");
+
+        test.end();
+    });
+
+});
