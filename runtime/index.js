0ea2740f0774b4c5c349b9c303f3fb2c2743c37b
dcodeIO <dcode@dcode.io>
2016-12-22 13:37:26 +0100

    Fixed: Fixed reference error in minimal runtime, see #580

--- runtime/index.js
+++ runtime/index.js
@@ -1,17 +1,18 @@
 // This file exports just the bare minimum required to work with statically generated code.
 // Can be used as a drop-in replacement for the full library as it has the same general structure.
+"use strict";
 var protobuf = exports;
 
-protobuf.Writer = require("../src/writer");
+protobuf.Writer       = require("../src/writer");
 protobuf.BufferWriter = require("../src/writer_buffer");
-protobuf.Reader = require("../src/reader");
+protobuf.Reader       = require("../src/reader");
 protobuf.BufferReader = require("../src/reader_buffer");
-protobuf.util = require("../src/util/runtime");
-protobuf.roots = {};
-protobuf.configure = configure;
+protobuf.util         = require("../src/util/runtime");
+protobuf.roots        = {};
+protobuf.configure    = configure;
 
 function configure() {
-    Reader._configure();
+    protobuf.Reader._configure();
 }
 
 // Be nice to AMD
