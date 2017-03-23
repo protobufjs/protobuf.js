404ba8e03a63f708a70a72f0208e0ca9826fe20b
dcodeIO <dcode@dcode.io>
2017-03-23 23:52:18 +0100

    Other: Just alias as the actual ideal type when using stubs, see #718

--- stub-long.d.ts
+++ stub-long.d.ts
@@ -1,10 +1,4 @@
 // minimal stub for Long instances for reference when not using long.js,
 // i.e. <reference path="node_modules/protobufjs/stub-long.d.ts" />
 
-type Long = LongStub;
-
-interface LongStub {
-    lo: number,
-    hi: number,
-    unsigned?: boolean
-}
+type Long = number;
--- stub-node.d.ts
+++ stub-node.d.ts
@@ -1,7 +1,4 @@
 // minimal stub for node types for reference when not using node,
 // i.e. <reference path="node_modules/protobufjs/stub-node.d.ts" />
 
-type Buffer = BufferStub;
-
-interface BufferStub extends Uint8Array {
-}
+type Buffer = Uint8Array;
