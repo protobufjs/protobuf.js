31bc076f690c133adec00a79e4df1ec26754002e
Pavel Studeny <studeny@avast.com>
2017-04-26 11:49:24 +0200

    rename fields to better mark what's going on

--- tests/comp_unknown-fields.js
+++ tests/comp_unknown-fields.js
@@ -3,13 +3,13 @@ var tape = require("tape");
 var protobuf = require("..");
 
 var proto = "message Simple_v1 {\
-  optional string name  = 1;\
-  optional string value = 3;\
+  optional string knownName  = 1;\
+  optional string knownValue = 3;\
 }\
 message Simple_v2 {\
-  optional string name  = 1;\
-  optional int32  flags = 2;\
-  optional string value = 3;\
+  optional string knownName  = 1;\
+  optional int32  unknownFlags = 2;\
+  optional string knownValue = 3;\
 }";
 
 var msg = { inner: [{}, {}, {}] };
@@ -19,14 +19,14 @@ tape.test("unknown fields", function (test) {
         Simple_v1 = root.lookup("Simple_v1");
         Simple_v2 = root.lookup("Simple_v2");
 
-    var s2 = Simple_v2.create({ name: "v2", flags: 2, value: "dummy" });
+    var s2 = Simple_v2.create({ knownName: "v2", unknownFlags: 2, knownValue: "dummy" });
     var s1 = Simple_v1.decode(Simple_v2.encode(s2).finish());
 
     var restored = Simple_v2.decode(Simple_v1.encode(s1).finish());
 
-    test.equal(s2.name, restored.name, "assert: even known fields are missing");
+    test.equal(s2.knownName, restored.knownName, "assert: even known fields are missing");
 
-    test.equal(2, restored.flags, "are preserved by default");
+    test.equal(2, restored.unknownFlags, "are preserved by default");
     test.end();
 });
 
@@ -35,7 +35,7 @@ tape.test("discarded unknown fields", function (test) {
         Simple_v1 = root.lookup("Simple_v1");
         Simple_v2 = root.lookup("Simple_v2");
 
-    var s2 = Simple_v2.create({ name: "v2", flags: 2, value: "dummy" });
+    var s2 = Simple_v2.create({ knownName: "v2", unknownFlags: 2, knownValue: "dummy" });
     var s1 = Simple_v1.decode(Simple_v2.encode(s2).finish());
 
     try {
@@ -48,46 +48,38 @@ tape.test("discarded unknown fields", function (test) {
 
     var restored = Simple_v2.decode(Simple_v1.encode(s1).finish());
 
-    test.equal(0, restored.flags, "are removed from the message");
+    test.equal(0, restored.unknownFlags, "are removed from the message");
     test.end();
 });
 
 tape.test("multiple unknown fields", function (test) {
     var proto = "message Simple_v1 {\
-      optional string name  = 1;\
-      optional string value = 3;\
+      optional string knownName  = 1;\
+      optional string knownValue = 3;\
     }\
     message Simple_v2 {\
-      optional string name  = 1;\
-      optional int32  flags = 2;\
-      optional string value = 3;\
-      optional int32  options = 4;\
+      optional string knownName  = 1;\
+      optional int32  unknownFlags = 2;\
+      optional string knownValue = 3;\
+      optional int32  unknownOptions = 4;\
     }";
 
     var root = protobuf.parse(proto).root,
         Simple_v1 = root.lookup("Simple_v1");
         Simple_v2 = root.lookup("Simple_v2");
 
-    var s2 = Simple_v2.create({ name: "v2", flags: 2, value: "dummy", options: 3 });
+    var s2 = Simple_v2.create({ knownName: "v2", unknownFlags: 2, knownValue: "dummy", unknownOptions: 3 });
     var s1 = Simple_v1.decode(Simple_v2.encode(s2).finish());
 
     var restored = Simple_v2.decode(Simple_v1.encode(s1).finish());
 
-    test.equal(2, restored.flags, "are preserved by default");
-    test.equal(3, restored.options, "are preserved by default");
+    test.equal(2, restored.unknownFlags, "(2) are preserved by default");
+    test.equal(3, restored.unknownOptions, "(4) are preserved by default");
     test.end();
 });
 
 tape.test("nested unknown fields", function (test) {
-    var proto = "message Simple_v1 {\
-      optional string name  = 1;\
-      optional string value = 3;\
-    }\
-    message Simple_v2 {\
-      optional string name  = 1;\
-      optional int32  flags = 2;\
-      optional string value = 3;\
-    }\
+    var nproto = proto + "\
     message Container_v1 {\
       optional Simple_v1 elem = 1;\
     }\
@@ -95,18 +87,18 @@ tape.test("nested unknown fields", function (test) {
       optional Simple_v2 elem = 1;\
     }";
 
-    var root = protobuf.parse(proto).root,
+    var root = protobuf.parse(nproto).root,
         Container_v1 = root.lookup("Container_v1");
         Container_v2 = root.lookup("Container_v2");
 
-    var c2 = Container_v2.create({ elem: { name: "v2", flags: 2, value: "dummy" } });
+    var c2 = Container_v2.create({ elem: { knownName: "v2", unknownFlags: 2, knownValue: "dummy" } });
     var c1 = Container_v1.decode(Container_v2.encode(c2).finish());
 
     var restored = Container_v2.decode(Container_v1.encode(c1).finish());
 
-    test.equal(c2.elem.name, restored.elem.name, "assert: even known fields are missing");
+    test.equal(c2.elem.knownName, restored.elem.knownName, "assert: even known fields are missing");
 
-    test.equal(2, restored.elem.flags, "are preserved by default");
+    test.equal(2, restored.elem.unknownFlags, "are preserved by default");
     test.end();
 });
 
