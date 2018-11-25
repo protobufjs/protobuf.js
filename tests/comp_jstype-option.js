c2c059abd9bf751bcc7e248049be3647c7639a2f
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2018-11-25 23:48:46 +0500

    Tests for JS_STRING option for single value were added

--- tests/comp_jstype-option.js
+++ tests/comp_jstype-option.js
@@ -18,3 +18,75 @@ tape.test("should handle jstype = JS_STRING for int64 field", function(test) {
     test.same(actualMessage, expectedMessage, "should encode and decode back to the original values");
     test.end();
 });
+
+tape.test("should handle jstype = JS_STRING for uint64 field", function(test) {
+    var proto = `
+    syntax = \"proto3\";
+    message Info {
+        uint64 id = 1 [jstype = JS_STRING];
+      }`;
+    var expectedMessage = {
+        id: "111111111111111"
+    };
+
+    const root = protobuf.parse(proto).root;
+    const Info = root.lookup("Info");
+    
+    var actualMessage = Info.decode(Info.encode(expectedMessage).finish());
+    test.same(actualMessage, expectedMessage, "should encode and decode back to the original values");
+    test.end();
+});
+
+tape.test("should handle jstype = JS_STRING for sint64 field", function(test) {
+    var proto = `
+    syntax = \"proto3\";
+    message Info {
+        sint64 id = 1 [jstype = JS_STRING];
+      }`;
+    var expectedMessage = {
+        id: "111111111111111"
+    };
+
+    const root = protobuf.parse(proto).root;
+    const Info = root.lookup("Info");
+    
+    var actualMessage = Info.decode(Info.encode(expectedMessage).finish());
+    test.same(actualMessage, expectedMessage, "should encode and decode back to the original values");
+    test.end();
+});
+
+tape.test("should handle jstype = JS_STRING for fixed64 field", function(test) {
+    var proto = `
+    syntax = \"proto3\";
+    message Info {
+        fixed64 id = 1 [jstype = JS_STRING];
+      }`;
+    var expectedMessage = {
+        id: "111111111111111"
+    };
+
+    const root = protobuf.parse(proto).root;
+    const Info = root.lookup("Info");
+    
+    var actualMessage = Info.decode(Info.encode(expectedMessage).finish());
+    test.same(actualMessage, expectedMessage, "should encode and decode back to the original values");
+    test.end();
+});
+
+tape.test("should handle jstype = JS_STRING for sfixed64 field", function(test) {
+    var proto = `
+    syntax = \"proto3\";
+    message Info {
+        sfixed64 id = 1 [jstype = JS_STRING];
+      }`;
+    var expectedMessage = {
+        id: "111111111111111"
+    };
+
+    const root = protobuf.parse(proto).root;
+    const Info = root.lookup("Info");
+    
+    var actualMessage = Info.decode(Info.encode(expectedMessage).finish());
+    test.same(actualMessage, expectedMessage, "should encode and decode back to the original values");
+    test.end();
+});
