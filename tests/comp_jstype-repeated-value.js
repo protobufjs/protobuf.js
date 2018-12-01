d7b8e45b640739d694503bfcb7d1f3a02e0125e9
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2018-12-01 19:13:57 +0500

    JS_NORMAL jstype option was tested for repeated fields

--- tests/comp_jstype-repeated-value.js
+++ tests/comp_jstype-repeated-value.js
@@ -180,3 +180,93 @@ tape.test("should handle jstype = JS_NUMBER for repeated sfixed64 field", functi
     test.same(actualMessage, expectedMessage, "should encode and decode back to the original values");
     test.end();
 });
+
+tape.test("should handle jstype = JS_NORMAL for repeated int64 field", function(test) {
+    var proto = `
+    syntax = \"proto3\";
+    message Info {
+        repeated int64 ids = 1 [jstype = JS_NORMAL];
+      }`;
+    var expectedMessage = {
+        ids: [protobuf.util.Long.fromNumber(11111111, false), protobuf.util.Long.fromNumber(222222222, false)]
+    };
+
+    const root = protobuf.parse(proto).root;
+    const Info = root.lookup("Info");
+    
+    var actualMessage = Info.decode(Info.encode(expectedMessage).finish());
+    test.same(actualMessage, expectedMessage, "should encode and decode back to the original values");
+    test.end();
+});
+
+tape.test("should handle jstype = JS_NORMAL for repeated uint64 field", function(test) {
+    var proto = `
+    syntax = \"proto3\";
+    message Info {
+        repeated uint64 ids = 1 [jstype = JS_NORMAL];
+      }`;
+    var expectedMessage = {
+        ids: [protobuf.util.Long.fromNumber(11111111, true), protobuf.util.Long.fromNumber(222222222, true)]
+    };
+
+    const root = protobuf.parse(proto).root;
+    const Info = root.lookup("Info");
+    
+    var actualMessage = Info.decode(Info.encode(expectedMessage).finish());
+    test.same(actualMessage, expectedMessage, "should encode and decode back to the original values");
+    test.end();
+});
+
+tape.test("should handle jstype = JS_NORMAL for repeated sint64 field", function(test) {
+    var proto = `
+    syntax = \"proto3\";
+    message Info {
+        repeated sint64 ids = 1 [jstype = JS_NORMAL];
+      }`;
+    var expectedMessage = {
+        ids: [protobuf.util.Long.fromNumber(11111111, false), protobuf.util.Long.fromNumber(222222222, false)]
+    };
+
+    const root = protobuf.parse(proto).root;
+    const Info = root.lookup("Info");
+    
+    var actualMessage = Info.decode(Info.encode(expectedMessage).finish());
+    test.same(actualMessage, expectedMessage, "should encode and decode back to the original values");
+    test.end();
+});
+
+tape.test("should handle jstype = JS_NORMAL for repeated fixed64 field", function(test) {
+    var proto = `
+    syntax = \"proto3\";
+    message Info {
+        repeated fixed64 ids = 1 [jstype = JS_NORMAL];
+      }`;
+    var expectedMessage = {
+        ids: [protobuf.util.Long.fromNumber(11111111, true), protobuf.util.Long.fromNumber(222222222, true)]
+    };
+
+    const root = protobuf.parse(proto).root;
+    const Info = root.lookup("Info");
+    
+    var actualMessage = Info.decode(Info.encode(expectedMessage).finish());
+    test.same(actualMessage, expectedMessage, "should encode and decode back to the original values");
+    test.end();
+});
+
+tape.test("should handle jstype = JS_NORMAL for repeated sfixed64 field", function(test) {
+    var proto = `
+    syntax = \"proto3\";
+    message Info {
+        repeated sfixed64 ids = 1 [jstype = JS_NORMAL];
+      }`;
+    var expectedMessage = {
+        ids: [protobuf.util.Long.fromNumber(11111111, false), protobuf.util.Long.fromNumber(222222222, false)]
+    };
+
+    const root = protobuf.parse(proto).root;
+    const Info = root.lookup("Info");
+    
+    var actualMessage = Info.decode(Info.encode(expectedMessage).finish());
+    test.same(actualMessage, expectedMessage, "should encode and decode back to the original values");
+    test.end();
+});
