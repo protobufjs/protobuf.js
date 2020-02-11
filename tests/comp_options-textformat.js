7b2988978c4f4cd8c471835c9e87613f9337e6cb
ab <batsuev@gmail.com>
2020-02-11 23:15:35 +0800

    enum valuesOptions support

--- tests/comp_options-textformat.js
+++ tests/comp_options-textformat.js
@@ -11,10 +11,16 @@ message MyOptions {\
 extend google.protobuf.FieldOptions {\
   MyOptions my_options = 50000;\
 }\
+extend google.protobuf.EnumValueOptions {\
+  MyOptions my_value_option = 50001;\
+}\
 message Test {\
   string value = 1 [(my_options) = { a: \"foo\" b: \"bar\" }];\
   string value2 = 2 [(my_options) = { a: \"foo\" b { c: \"bar\" } }];\
   string value3 = 3 [(my_options) = { a: \"foo\", b: \"bar\" }];\
+}\
+enum TestEnum {\
+  TEST_ITEM = 0 [(my_value_option) = { a: \"foo\", b: \"bar\" }];\
 }";
 
 tape.test("options in textformat", function(test) {
@@ -24,4 +30,7 @@ tape.test("options in textformat", function(test) {
     test.same(Test.fields.value2.options, { "(my_options).a": "foo", "(my_options).b.c": "bar" }, "should parse correctly when nested");
     test.same(Test.fields.value3.options, { "(my_options).a": "foo", "(my_options).b": "bar" }, "should parse correctly when comma-separated");
     test.end();
+
+    var TestEnum = root.lookup("TestEnum");
+    test.same(TestEnum.valuesOptions["TEST_ITEM"], { "(my_value_option).a": "foo", "(my_value_option).b": "bar" }, "should parse correctly in enum");
 });
