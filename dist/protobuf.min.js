81bfd96afa9308a28a1de29b332c442510f1d739
Benjamin E. Coe <bencoe@google.com>
2020-04-17 15:04:06 -0700

    build: run make (#1391)

--- dist/protobuf.min.js
+++ dist/protobuf.min.js
@@ -1,6 +1,6 @@
 /*!
  * protobuf.js v6.8.9 (c) 2016, daniel wirtz
- * compiled thu, 16 apr 2020 14:35:35 utc
+ * compiled fri, 17 apr 2020 21:58:51 utc
  * licensed under the bsd-3-clause license
  * see: https://github.com/dcodeio/protobuf.js for details
  */
