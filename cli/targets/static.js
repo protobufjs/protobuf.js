b978c1557b1dd0e8a28a03a1f4e3591e265584f3
yangjun <290968698@qq.com>
2020-06-13 16:00:54 +0800

    fix package name has multiple level

--- cli/targets/static.js
+++ cli/targets/static.js
@@ -129,7 +129,7 @@ function buildNamespace(ref, ns) {
             ns.parent instanceof protobuf.Root ? "@exports " + escapeName(ns.name) : "@memberof " + exportName(ns.parent),
             "@namespace"
         ]);
-        push((config.es6 ? "const" : "var") + " " + escapeName(ns.name) + " = $root." + escapeName(ns.name) + " || {};");
+        push((config.es6 ? "const" : "var") + " " + escapeName(ns.name) + " = $root." + (ns.parent instanceof protobuf.Root ? escapeName(ns.name) : (exportName(ns.parent) + '.' + escapeName(ns.name))) + " || {};");
     }
 
     ns.nestedArray.forEach(function(nested) {
