0fd55389382b78e2fa3a7ff44cd34e77a0be124e
Ben Delarre <delarre@adobe.com>
2021-06-09 18:33:07 -0700

    Fixes #1230, adding ES Module output

--- dist/minimal/protobuf.js
+++ dist/minimal/protobuf.js
@@ -1,6 +1,6 @@
 /*!
- * protobuf.js v6.10.0 (c) 2016, daniel wirtz
- * compiled wed, 15 jul 2020 23:34:13 utc
+ * protobuf.js v6.10.2 (c) 2016, daniel wirtz
+ * compiled thu, 10 jun 2021 01:21:17 utc
  * licensed under the bsd-3-clause license
  * see: https://github.com/dcodeio/protobuf.js for details
  */
@@ -1334,7 +1334,7 @@ module.exports = {};
 /**
  * Named roots.
  * This is where pbjs stores generated structures (the option `-r, --root` specifies a name).
- * Can also be used manually to make roots available accross modules.
+ * Can also be used manually to make roots available across modules.
  * @name roots
  * @type {Object.<string,Root>}
  * @example
