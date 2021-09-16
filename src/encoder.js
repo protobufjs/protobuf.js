0b7c61eb9b18ddcfd9a9c6192658a1376aa05e72
Balazs Ree <ree@greenfinity.hu>
2021-09-16 11:23:18 +0200

    Fix encoding of int64 keys of maps in encoder
    The int64 writer cannot handle the hash directly, because it interprets
    it as a string representation of a Long number, instead of a hash.
    Explicit conversion from hash to long is needed before writing.

--- src/encoder.js
+++ src/encoder.js
@@ -48,7 +48,7 @@ function encoder(mtype) {
             gen
     ("if(%s!=null&&Object.hasOwnProperty.call(m,%j)){", ref, field.name) // !== undefined && !== null
         ("for(var ks=Object.keys(%s),i=0;i<ks.length;++i){", ref)
-            ("w.uint32(%i).fork().uint32(%i).%s(ks[i])", (field.id << 3 | 2) >>> 0, 8 | types.mapKey[field.keyType], field.keyType);
+            ("w.uint32(%i).fork().uint32(%i).%s(%s)", (field.id << 3 | 2) >>> 0, 8 | types.mapKey[field.keyType], field.keyType, field.keyType === "int64" ? "$util.longFromHash(keys[i], false)" : "ks[i]");
             if (wireType === undefined) gen
             ("types[%i].encode(%s[ks[i]],w.uint32(18).fork()).ldelim().ldelim()", index, ref); // can't be groups
             else gen
