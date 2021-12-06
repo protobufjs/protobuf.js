5e38c52831017c159adb410a144c8743306ff056
kurthe <kurthe@tencent.com>
2021-12-06 17:05:35 +0800

    feat: support map key use Long.toString when is Long  #1203

--- src/decoder.js
+++ src/decoder.js
@@ -72,7 +72,7 @@ function decoder(mtype) {
                 ("}");
 
             if (types.long[field.keyType] !== undefined) gen
-                ("%s[typeof k===\"object\"?util.longToHash(k):k]=value", ref);
+                ("%s[util.Long.isLong(k)?k.toString():typeof k===\"object\"?util.longToHash(k):k]=value", ref);
             else gen
                 ("%s[k]=value", ref);
 
