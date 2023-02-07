14f05364a04fe1ca0bfb278b3407e058c6b5a1ab
Petr Plenkov <petr.plenkov@gmail.com>
2023-02-07 20:48:01 +0530

    fix: do not allow to extend same field twice to prevent the error (#1784)
    * do not allow to extend same field twice to prevent the error
    
    * Ignore gitpod config
    
    * unit test for issue #1783
    
    * using existing test file

--- src/root.js
+++ src/root.js
@@ -274,6 +274,10 @@ function tryHandleExtension(root, field) {
     var extendedType = field.parent.lookup(field.extend);
     if (extendedType) {
         var sisterField = new Field(field.fullName, field.id, field.type, field.rule, undefined, field.options);
+        //do not allow to extend same field twice to prevent the error
+        if (extendedType.get(sisterField.name)) {
+            return true;
+        }
         sisterField.declaringField = field;
         field.extensionField = sisterField;
         extendedType.add(sisterField);
