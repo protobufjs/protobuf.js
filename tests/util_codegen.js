46caf314770ac0489d4bf6ea04bd1f8f2926dbfc
dcode <dcode@dcode.io>
2026-07-27 11:16:12 +0200

    fix: Preserve negative zero in codegen formatters

--- tests/util_codegen.js
+++ tests/util_codegen.js
@@ -13,5 +13,17 @@ tape.test("codegen", function(test) {
 
     test.equal(add(1, 2), 3, "should generate a working function");
 
+    var floats = codegen()
+      ("return [%f,%f,%f,%f,%f,%d]", 1.5, Infinity, -Infinity, NaN, -0, -0)
+      ()
+      ();
+
+    test.equal(floats[0], 1.5, "should format finite floating-point values");
+    test.equal(floats[1], Infinity, "should format positive infinity");
+    test.equal(floats[2], -Infinity, "should format negative infinity");
+    test.ok(Number.isNaN(floats[3]), "should format NaN");
+    test.ok(Object.is(floats[4], -0), "should format negative zero");
+    test.ok(Object.is(floats[5], -0), "should format negative zero as a number");
+
     test.end();
 });
